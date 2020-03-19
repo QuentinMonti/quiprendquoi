48f165f6123428d9f938380f8fa5d8e66a87ef0e
Quentin <quentin.monteferrario@gmail.com>
2020-03-19 20:04:32 +0100

    Share API

---
+++ scripts/share.js
@@ -0,0 +1,21 @@
+if (navigator.share) {
+    document.querySelectorAll('[data-share-url]').forEach(($shareEl) => {
+        const $button = document.createElement('button');
+        $button.innerHTML = 'Partager';
+        $shareEl.parentNode.append($button);
+
+        $button.addEventListener(
+            'click',
+            share.bind(this, $shareEl));
+    });
+} else {
+    console.warn("Pas de partage");
+}
+
+function share($shareEl) {
+    navigator.share({
+        title: $shareEl.getAttribute('data-share-title'),
+        text: $shareEl.getAttribute('data-share-text'),
+        url: $shareEl.getAttribute('data-share-url'),
+    });
+}
